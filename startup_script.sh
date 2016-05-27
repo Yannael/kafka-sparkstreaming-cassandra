344d8c4ae41df56fca5b04e342ab6343b7e8f3df
yannael <yannael@gmail.com>
2016-05-27 11:01:27 +0200

    Kafka and Zookeeper started from kafka/bin

--- startup_script.sh
+++ startup_script.sh
@@ -3,5 +3,5 @@
 service sshd start
 service cassandra start
 
-su guest zookeeper-server-start.sh $HOME/kafka/config/zookeeper.properties  > /home/guest/zookeeper.log 2>&1 &
-su guest kafka-server-start.sh $HOME/kafka/config/server.properties > /home/guest/kafka.log 2>&1 &
+su guest $HOME/kafka/bin/zookeeper-server-start.sh $HOME/kafka/config/zookeeper.properties  > /home/guest/zookeeper.log 2>&1 &
+su guest $HOME/kafka/bin/kafka-server-start.sh $HOME/kafka/config/server.properties > /home/guest/kafka.log 2>&1 &
