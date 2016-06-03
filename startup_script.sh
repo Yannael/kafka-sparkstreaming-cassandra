0d285ea418f6eb29945bd13175183c3afbc81486
yannael <yannael@gmail.com>
2016-06-03 15:27:59 +0200

    Create Cassandra table

--- startup_script.sh
+++ startup_script.sh
@@ -5,3 +5,6 @@ service cassandra start
 
 su guest $HOME/kafka/bin/zookeeper-server-start.sh $HOME/kafka/config/zookeeper.properties  > /home/guest/zookeeper.log 2>&1 &
 su guest $HOME/kafka/bin/kafka-server-start.sh $HOME/kafka/config/server.properties > /home/guest/kafka.log 2>&1 &
+
+sleep 15
+cqlsh -f init_cassandra.cql
